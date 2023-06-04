8c115eeeb76f5fcd20a1d0187b3c0bfab6957734
Aayushnepal11 <neymarnepal82@gmail.com>
2023-06-04 19:43:01 +0545

    Fix Code Issues

--- api_fetcher/views.py
+++ api_fetcher/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from .gmail_api import GmailAPI
-# from django.contrib import messages
-import csv
+from django.contrib import messages
+
 
 GMAIL_DATA = GmailAPI()
 
-INBOX_DATA = GMAIL_DATA.data_getter("me", 5, "inbox")
-SPAM_DATA = GMAIL_DATA.data_getter("me", 10, "spam")
-DRAFT_DATA = GMAIL_DATA.data_getter("me", 5, "draft")
+INBOX_DATA = GMAIL_DATA.data_getter("me", 100, "inbox")
+SPAM_DATA = GMAIL_DATA.data_getter("me", 50, "spam")
 
-# INBOX_DATA_LENGTH = len(INBOX_DATA)
-# SPAM_DATA_LENGTH = len(SPAM_DATA)
-# DRAFT_DATA_LENGTH = len(DRAFT_DATA)
 
-# length_list = [INBOX_DATA_LENGTH, SPAM_DATA_LENGTH, DRAFT_DATA]
+INBOX_DATA_LENGTH = len(INBOX_DATA)
+SPAM_DATA_LENGTH = len(SPAM_DATA)
+
 
 def data_fetcher(data):
     data_list = list()
@@ -39,42 +37,32 @@ def data_fetcher(data):
 
 def index(request):
     context = {
-        'title': 'Spam Checker:Home'
+        'title': 'Spam Checker:Home',
+        'inbox_count': INBOX_DATA_LENGTH,
+        'spam_count': SPAM_DATA_LENGTH,
         }
     return render(request, 'gmail_api/index.html', context)
 
 
 
-# def inbox(request):
-#     if INBOX_DATA_LENGTH == 0:
-#         messages.info(request, "You don't have anything on your inbox.")
+def inbox(request):
+    if INBOX_DATA_LENGTH == 0:
+        messages.info(request, "You don't have anything on your inbox.")
 
-#     context = {
-#         'title': 'Spam_Checker/Inbox',
-#         'mail_box_data': length_list,
-#         'fetch_data': data_fetcher(INBOX_DATA),
-#     }
-#     return render(request, 'gmail_api/index.html', context)
-
-
-# def spam(request):
-#     if SPAM_DATA_LENGTH == 0:
-#         messages.info(request, "You don't have anything on your spam.")
-#     context = {
-#         'title': 'Spam_Checker/Spam',
-#         'mail_box_data': length_list,
-#         'fetch_data': data_fetcher(SPAM_DATA)
-#     }
-#     HttpResponseRedirect(redirect_to='spam_checker:home')
-#     return render(request, 'gmail_api/spam.html', context)
+    context = {
+        'title': 'Spam_Checker/Inbox',
+        'fetch_data': data_fetcher(INBOX_DATA),
+    }
+    return render(request, 'gmail_api/inbox.html', context)
 
 
-# def draft(request):
-#     if DRAFT_DATA_LENGTH == 0:
-#         messages.info(request, "You don't have anything on your draft.")
-#     context = {
-#         'title': 'Spam_checker/Draft',
-#         'fetch_data': data_fetcher(DRAFT_DATA),
-#         'mail_box_data': length_list
-#     }
-#     return render(request, 'gmail_api/drafts.html', context)
+def spam(request):
+    if SPAM_DATA_LENGTH == 0:
+        messages.info(request, "You don't have anything on your spam.")
+    context = {
+        'title': 'Spam_Checker/Spam',
+        'fetch_data': data_fetcher(SPAM_DATA)
+    }
+    return render(request, 'gmail_api/spam.html', context)
+
+
